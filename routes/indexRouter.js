62215f1b3c5350e24fea1d12887ed4caf44878cd
ivanovichi <ivan.arthur18@gmail.com>
2026-08-10 10:01:26 -0600

    feat: Added mock messages

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,5 +1,23 @@
 import { Router } from "express";
 
+const messages = [
+    {
+        text: "Hi there!",
+        user: "Armando",
+        added: new Date()
+    },
+    {
+        text: "Hello world!",
+        user: "Charles",
+        added: new Date()
+    },
+    {
+        text: "Awesome server!",
+        user: "The creator",
+        added: new Date()
+    }
+];
+
 const indexRouter = Router();
 
 indexRouter.get(["/"], (req, res) => {
